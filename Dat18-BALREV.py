0203cd3b0dc548cac5e8b3be71f74a6a6a3aa840
Bloody-fangg <162534666+Bloody-fangg@users.noreply.github.com>
2024-10-20 21:35:36 +0530

    Add files via upload

---
+++ Dat18-BALREV.py
@@ -0,0 +1,6 @@
+# cook your dish here
+# cook your dish here
+for _ in range(int(input())):
+    n=int(input())
+    l=input()
+    print("".join(sorted(l)))
---
+++ Day18-BIN_BAT.py
@@ -0,0 +1,10 @@
+# cook your dish here
+# cook your dish here
+for I in range(int(input())):
+    a,b,c=map(int,input().split())
+    f=-1
+    while(a):
+        a//=2
+        f+=1
+    k=f*b+(f-1)*c
+    print(k)
