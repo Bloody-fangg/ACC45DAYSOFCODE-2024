d4e2b43b225325c40ca309f282fb4cc1a7c6c374
Bloody-fangg <162534666+Bloody-fangg@users.noreply.github.com>
2024-10-25 21:04:19 +0530

    Add files via upload

---
+++ Day23-REMOVEBAD.py
@@ -0,0 +1,14 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    N=int(input())
+    l=list(map(int,input().split()))
+    d={}
+    for k in l:
+        if k in d:
+            d[k]+=1
+        else:
+            d[k]=1
+    m=max(d.values())
+    print(N-m)
+        
