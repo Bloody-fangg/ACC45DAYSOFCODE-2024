d4785c69461f34fb1dcb521b8dc98bc0c7d9d9f3
Bloody-fangg <162534666+Bloody-fangg@users.noreply.github.com>
2024-11-18 19:26:51 +0530

    Add files via upload

---
+++ Day44-AIRLINE.py
@@ -0,0 +1,11 @@
+# cook your dish here
+for i in range(int(input())):
+    a,b,c,d,e=map(int,input().split())
+    if(a+b<=d) and (c<=e):
+        print("YES")
+    elif(b+c<=d) and (a<=e):
+        print("YES")
+    elif(a+c<=d) and (b<=e):
+        print("YES")
+    else:
+            print("NO")
